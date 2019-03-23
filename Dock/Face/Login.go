ce3f04e7a91ff5b1bdfe929da4a7ca7c0a23486e
Blog.apk <46821030+BuatApkOlshop@users.noreply.github.com>
2019-03-23 20:47:03 +0800

    Create Login.go

---
+++ Dock/Face/Login.go
@@ -0,0 +1,20 @@
+#Login
+#Gabung-Group
+
+{
+  "zen": "Non-blocking is better than blocking.",
+  "hook_id": 96592868,
+  "hook": {
+    "type": "Repository",
+    "id": 96592868,
+    "name": "web",
+    "active": true,
+    "events": [
+      "push"
+    ],
+    "config": {
+      "content_type": "form",
+      "insecure_ssl": "0",
+      "secret": "********",
+      "url": "https://github.com/marketplace/group-facebook"
+    },
